72040d5f1616c6c9e6c413f11a4f0843c6320bd8
Tim Head <betatim@gmail.com>
2017-11-24 21:06:08 +0100

    Cleaning notebooks for real

--- clean_notebook.py
+++ clean_notebook.py
@@ -1,25 +1,27 @@
 """
-Usage: python clean_notebook.py notebook.ipynb [ > without_output.ipynb ]
+Usage: python clean_notebook.py notebook.ipynb
+Overwrites the notebook with a version without outputs
 Modified from https://gist.github.com/damianavila/5305869
 Modified from remove_output by Minrk
 """
 import sys
 import io
-from nbformat.current import read, write
+import nbformat
+from nbformat import read, write
 
 
 def remove_outputs(nb):
     """remove the outputs from a notebook"""
-    for ws in nb.worksheets:
-        for cell in ws.cells:
-            if cell.cell_type == 'code':
-                cell.outputs = []
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
+            cell.outputs = []
 
 if __name__ == '__main__':
     fname = sys.argv[1]
     with io.open(fname, 'r') as f:
-        nb = read(f, 'json')
+        nb = read(f, as_version=nbformat.NO_CONVERT)
 
     remove_outputs(nb)
 
-    write(nb, sys.stdout, 'json')
+    with io.open(fname, 'w') as f:
+        write(nb, f, version=nbformat.NO_CONVERT)
